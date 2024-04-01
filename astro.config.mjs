cd1eebde17c997e8ec9ca11e135362bf54c76579
OdiseoRincon@gmail.com <OdiseoRincon@gmail.com>
2024-04-01 10:47:18 -0400

    arreglando build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  build: {
+    format: 'file',
+    assets: 'static-dev'
+  },
+  output: 'static',
+  outDir: './dist',
+  base: '/',
+  integrations: [tailwind()],
+  adapter: netlify()
 });
---
+++ src/Data/Proyectos.ts
@@ -0,0 +1,32 @@
+export const proyectos = [
+    {
+      title: "Sistema de gestion de recursos humanos",
+      description:
+        "Este es un proyecto hecho con NET y Angular, este sistema simula lo que es la administracion de empleados desde el punto de vista de una persona de RRHH",
+      
+      github: "https://github.com/odiseo153/Sistema_Gestion_Recursos_Humanos",
+      image:
+       "/hhrr.png" ,  
+    },
+   
+    {
+      title: "Book Store Astro",
+      description:
+        "este es un proyecto en astro que simula funciones de una libreria, aqui usamos el tipico crud, los datos son de un json que esta en el proyecto",
+      github: "https://github.com/odiseo153/Book-Store-Astro",
+      link: "https://book-store-odiseo.netlify.app/",
+      image:
+      "/libros.png",
+    }, 
+  
+    {
+      title: "Poke Api",
+      description:
+        "este es un proyecto en angular en donde hago la tipica poke api, aqui se puede buscar pokemon por nombre lo cual es muy util",
+      github: "https://github.com/odiseo153/PokeApi_Angular.git",
+      link: "https://pokedex-odiseo.netlify.app/",
+      image:
+      "/pokemon.png",
+    }, 
+  
+  ];
