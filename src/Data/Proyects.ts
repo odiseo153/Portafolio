264f417aeb8b2c12f7c80125daacb58036f63e1d
OdiseoRincon@gmail.com <OdiseoRincon@gmail.com>
2024-04-01 22:46:35 -0400

    agregando navbar

---
+++ src/Data/Proyects.ts
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+      title: "Human Resources Management System",
+      description:
+        "This is a project built with .NET and Angular. This system simulates employee management from the perspective of an HR person.",
+      github: "https://github.com/odiseo153/Sistema_Gestion_Recursos_Humanos",
+      image: "/hhrr.png",
+    },
+    {
+      title: "Book Store Astro",
+      description:
+        "This is a project in Astro that simulates library functions. Here, we use the typical CRUD operations. The data comes from a JSON file within the project.",
+      github: "https://github.com/odiseo153/Book-Store-Astro",
+      link: "https://book-store-odiseo.netlify.app/",
+      image: "/libros.png",
+    },
+    {
+      title: "Poke API",
+      description:
+        "This is a project in Angular where I create the typical Poke API. Here, you can search for Pokémon by name, which is very useful.",
+      github: "https://github.com/odiseo153/PokeApi_Angular.git",
+      link: "https://pokedex-odiseo.netlify.app/",
+      image: "/pokemon.png",
+    },
+  ];
+  
